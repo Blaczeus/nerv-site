8fb27dad4a70dbf98d526fa25d41d9a23fef6659
blaczeus029 <anyachidiebere029@gmail.com>
2026-01-12 07:47:31 +0100

    refactor: remove unused breadcrumbs and comment out AppLayout usage in Dashboard component

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -5,18 +5,13 @@ import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
-const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard().url,
-    },
-];
+
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <AppLayout :breadcrumbs="breadcrumbs">
+    <!-- <AppLayout :breadcrumbs="breadcrumbs"> -->
         <div
             class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
         >
@@ -43,5 +38,5 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <PlaceholderPattern />
             </div>
         </div>
-    </AppLayout>
+    <!-- </AppLayout> -->
 </template>
